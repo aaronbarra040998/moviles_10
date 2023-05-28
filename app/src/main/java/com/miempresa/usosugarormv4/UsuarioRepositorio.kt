5ecb9504e673f1dd706a45bd4e174759bca5db9e
aaronbarra040998 <aaron.apaza@tecsup.edu.pe>
2023-05-27 18:18:23 -0600

    moviles

---
+++ app/src/main/java/com/miempresa/usosugarormv4/UsuarioRepositorio.kt
@@ -0,0 +1,28 @@
+package com.miempresa.usosugarormv4
+import com.orm.SugarRecord
+
+class UsuarioRepositorio {
+    fun crear (nombre:String,correo:String,clave:String){
+        var usuario = Usuario(nombre,correo,clave)
+        SugarRecord.save(usuario)
+    }
+    fun listar () :List<Usuario>{
+        var usuario=SugarRecord.listAll(Usuario::class.java)
+        return usuario
+    }
+    fun borrar(id:Long){
+        var usuario:Usuario = SugarRecord.findById(Usuario::class.java,id)
+        SugarRecord.delete(usuario)
+    }
+    fun leer(id: Long):Usuario{
+        val usuario: Usuario = SugarRecord.findById(Usuario::class.java,id)
+        return usuario
+    }
+    fun actualizar(id: Long,nombre: String,correo: String,clave: String){
+        var usuario: Usuario = SugarRecord.findById(Usuario::class.java,id)
+        usuario.nombre = nombre
+        usuario.correo = correo
+        usuario.clave = clave
+        SugarRecord.save(usuario)
+    }
+}
